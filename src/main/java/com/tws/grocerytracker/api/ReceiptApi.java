44c32a9ef183e74f12e324814102d96fd8ba1a95
Travis Svendsen <travis.svendsen@gmail.com>
2026-09-12 13:42:59 -0500

    import cleanup

--- src/main/java/com/tws/grocerytracker/api/ReceiptApi.java
+++ src/main/java/com/tws/grocerytracker/api/ReceiptApi.java
@@ -5,9 +5,11 @@ import com.tws.grocerytracker.service.ReceiptService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Pattern;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
